65ccb5ce7e9cf71dd1705abcad67c8b06e639033
azizsige <kurniawan00azizsige@gmail.com>
2022-09-26 14:31:32 +0700

    add: endpoint API

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
+API_IMAGE = "https://image.tmdb.org/t/p/w500";
+API_KEY = "0407909af3830d4ca4556ca68266735f";
+API_TRENDING = `https://api.themoviedb.org/3/trending/all/day?${API_KEY}`;
+API_MOVIES_NOW = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
+API_MOVIES_POPULAR = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+API_MOVIES_UPCOMING = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
+API_TV_POPULAR = `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`;
+API_TV_AIRING = `https://api.themoviedb.org/3/tv/airing_today?api_key=${API_KEY}&language=en-US&page=1`;
+API_TV_TOP = `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
+
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.min.css";
 import "owl.carousel/dist/owl.carousel.min.js";
