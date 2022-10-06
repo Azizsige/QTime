03f3938389b6ddcd647be3203c6e242412c8473d
azizsige <kurniawan00azizsige@gmail.com>
2022-10-06 18:40:57 +0700

    test: url issue part 10

--- src/js/app.js
+++ src/js/app.js
@@ -383,7 +383,7 @@ allCont.addEventListener("click", function (el) {
   const dataParse = JSON.stringify(storeTranding);
   localStorage.setItem("dataTrending", dataParse);
   if (parseInt(target.parentElement.id) == storeTranding[0].id) {
-    if (filename == " ") {
+    if (filename.length === 0 && filename == "index.html") {
       window.location = "./pages/detail.html";
       alert("yeyyy");
     } else {
@@ -414,7 +414,7 @@ moviesCont.addEventListener("click", function (el) {
   const dataParse = JSON.stringify(storeTranding);
   localStorage.setItem("dataTrending", dataParse);
   if (parseInt(target.parentElement.id) == storeTranding[0].id) {
-    if (filename == " ") {
+    if (filename.length === 0 && filename == "index.html") {
       window.location = "./pages/detail.html";
       // alert("yeyyy");
     } else {
@@ -445,7 +445,7 @@ tvsCont.addEventListener("click", function (el) {
   const dataParse = JSON.stringify(storeTranding);
   localStorage.setItem("dataTrending", dataParse);
   if (parseInt(target.parentElement.id) == storeTranding[0].id) {
-    if (filename == " ") {
+    if (filename.length === 0 && filename == "index.html") {
       window.location = "./pages/detail.html";
       // alert("yeyyy");
     } else {
